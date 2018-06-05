ee48140349a765b7047c76111dbec1bf1d932260
Takahiro Nakamura <dxbxdxbxdxb+dev@gmail.com>
2018-06-05 19:30:01 +0900

    try subscribe

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-import expect from 'expect'
 import { createStore } from 'redux'
 
 const counter = (state = 0, action) => {
@@ -14,8 +13,10 @@ const counter = (state = 0, action) => {
 
 const store = createStore(counter)
 
-console.log(store.getState())
+store.subscribe(() => {
+    document.body.innerText = store.getState()
+})
 
-store.dispatch({ type: 'INCREMENT' })
-
-console.log(store.getState())
+document.addEventListener('click', () => {
+    store.dispatch({ type: 'INCREMENT' })
+})
